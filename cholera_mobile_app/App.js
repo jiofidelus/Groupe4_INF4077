9003cbe12110802a7bda8d5b38be18d90495dff8
atemengue <moafembe@gmail.com>
2020-12-11 12:46:46 +0100

    add simple login for getting started

--- cholera_mobile_app/App.js
+++ cholera_mobile_app/App.js
@@ -6,72 +6,91 @@
  * @flow strict-local
  */
 
+import * as eva from '@eva-design/eva';
+import {
+  ApplicationProvider,
+  Button,
+  Input,
+  Layout,
+} from '@ui-kitten/components';
 import React from 'react';
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+const HomeScreen = () => (
+  <Layout style={{display: 'flex', flex: 1, margin: 25}}>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text style={{fontSize: 18, fontFamily: 'Ranchers-Regular'}}>
+        CHOLERA MONITORING{' '}
+      </Text>
+    </View>
+    <View style={{flex: 2}}>
+      <Text style={{fontSize: 16}}>Connectez vous a votre compte</Text>
+      <Input size="large" style={styles.inputStyle} placeholder="email" />
+      <Input
+        size="large"
+        style={styles.inputStyle}
+        secureTextEntry
+        placeholder="mot de passe"
+      />
+      <Button> Connectez vous</Button>
+    </View>
+    <View style={{flex: 1}}>
+      <Text>Ou connectez vous avec</Text>
+      <View
+        style={{
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+        }}>
+        <TouchableOpacity style={styles.button}>
+          <Image source={require('./src/assets/images/001-facebook.png')} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} appearance="ghost">
+          <Image source={require('./src/assets/images/002-google.png')} />
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button} appearance="ghost">
+          <Image source={require('./src/assets/images/003-twitter.png')} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  </Layout>
+);
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <View>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-        <Text>REGIS ATEMENGUE</Text>
-      </View>
+      <ApplicationProvider {...eva} theme={eva.light}>
+        <HomeScreen />
+      </ApplicationProvider>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  inputStyle: {
+    marginVertical: 10,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+
+  button: {
+    padding: 20,
+    marginTop: 15,
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    shadowColor: 'red',
   },
 });
 
