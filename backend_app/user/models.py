3587fef39763ed083c44b2632bb6c7b8465e94d6
BonarchiSimeu <bonarchisimeu@gmail.com>
2020-12-13 11:30:36 +0100

    Update user service

--- backend_app/user/models.py
+++ backend_app/user/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.Model):
+class UserProfil(models.Model):
     user = models.OneToOneField(
         'auth.User', related_name='profil', on_delete=models.CASCADE)
-
+    medicalStructure = models.ForeignKey(
+        'MedicalStructure', default=None, on_delete=models.CASCADE)
     photo = models.ImageField(null=True, upload_to='photos/')
     contact = models.IntegerField()
     address = models.TextField()
+
+
+class MedicalStructure(models.Model):
+    structureName = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    description = models.TextField()
